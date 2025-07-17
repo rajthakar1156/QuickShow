7bcc19a99e3e0d679bf310d2b3d50c00017dcd3f
rajthakar1156 <rajthakar1156@gmail.com>
2025-07-17 22:36:24 +0530

    feat: add show management APIs and models for booking, movie, and show

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ import connectDB from './configs/db.js';
 import { clerkMiddleware } from '@clerk/express'
 import { serve } from "inngest/express";
 import { inngest, functions } from "./inngest/index.js"
+import showRouter from './routes/showRoutes.js';
 
 const app=express();
 const port=process.env.PORT || 3000;
@@ -24,7 +25,7 @@ app.get('/',(req,res)=>{
 })
 
 app.use('/api/inngest',serve({client:inngest,functions}))
-
+app.use('/api/show',showRouter)
 app.listen(port,()=>{
     console.log(`server is listening at port:${port}`)
 })
