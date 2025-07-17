1f01ed3b11b2ab284b7bd6ccadd9b6a179e19a72
rajthakar1156 <rajthakar1156@gmail.com>
2025-07-17 19:21:19 +0530

    feat: integrate Clerk and Inngest for user management
    - Added @clerk/express and inngest dependencies to package.json
    - Implemented Clerk middleware in server.js for authentication
    - Created Inngest functions for syncing user creation, deletion, and updates with Clerk
    - Added User model for MongoDB to store user data
    - Configured Vercel settings for both client and server
    - Added vercel.json for client with rewrites and server with builds and routes

--- server/configs/db.js
+++ server/configs/db.js
@@ -1,1 +1,15 @@
-import mongoose from 'mongoose'
+import mongoose from "mongoose";
+
+ const connectDB = async () => {
+  try {
+    mongoose.connection.on("connected", () =>
+      console.log("Database Connected")
+    );
+    await mongoose.connect(`${process.env.MONGODB_URL}/QuickShow`);
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+export default connectDB
+
