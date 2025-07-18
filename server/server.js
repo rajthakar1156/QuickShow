6b794e66db42bdfa7b10820d607bd18506a4c045
rajthakar1156 <rajthakar1156@gmail.com>
2025-07-18 18:12:52 +0530

    feat: add user routes for managing user-related APIs

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ import { inngest, functions } from "./inngest/index.js"
 import showRouter from './routes/showRoutes.js';
 import bookingRouter from './routes/bookingRoutes.js';
 import adminRouter from './routes/adminRoutes.js';
+import userRouter from './routes/userRoutes.js';
 
 const app=express();
 const port=process.env.PORT || 3000;
@@ -30,6 +31,7 @@ app.use('/api/inngest',serve({client:inngest,functions}))
 app.use('/api/show',showRouter)
 app.use('/api/booking',bookingRouter)
 app.use('/api/admin',adminRouter)
+app.use('/api/user',userRouter)
 
 
 
